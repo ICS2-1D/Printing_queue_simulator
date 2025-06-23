8f04b1f261ab4ea612624acf166adaca1f041a84
Jeremiah <sentomerojeremy@gmail.com>
2025-06-23 23:35:26 +0300

    Refactor PrintQueue initialization to include capacity, aging interval, and expiry time

--- printQueue.py
+++ printQueue.py
@@ -1,3 +1,15 @@
 class PrintQueue:
-    def __init__(self):
-        self.queue = []
+      def __init__(self, capacity=10, aging_interval=5, expiry_time=30):
+         self.queue = [None] * capacity
+         self.capacity = capacity
+         self.front = 0
+         self.rear = 0
+         self.size = 0
+         self.aging_interval = aging_interval
+         self.expiry_time = expiry_time
+         
+         
+         
+          
+        
+        
