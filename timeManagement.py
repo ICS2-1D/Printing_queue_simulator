73d60d4986f27b28408076a1a2b18327176a2e7e
Jolene Sitienei <sitieneijolene@gmail.com>
2025-06-24 10:13:34 +0300

    implement placeholder priority and aging system for module 2

--- timeManagement.py
+++ timeManagement.py
@@ -2,3 +2,16 @@
 def __init__(self):
     self.queue =[]
     self.current_time = 0
+
+##PLACEHOLDER FUNCTIONS
+#module 2 - Priority and aging system
+def apply_priority_aging(self):
+    print("Aging jobs...")
+    for job in self.queue:
+        if job.waiting_time > 5:
+            job.priority += 1
+            print(f"Job {job.job_id} priority increased to {job.priority}")
+
+##reminder to check waiting list and age every 10 min
+def should_age_now(self):
+    return self.current_time % 5 == 0
